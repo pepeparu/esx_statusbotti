7c58cfacd78b60dd101b59819221f7e364fae955
El Pepe <76623780+pepeparu@users.noreply.github.com>
2022-01-27 17:37:11 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+
+---Viesti
+Config.Webhookki = "" --Laita webhookki tähän
+Config.Viesti = "" ---Viesti ID. Viestin pitää olla yllä olevan webhookin lähettämä
+
+----Embedi
+Config.Vari = 3447003 -- Värejä löydät mm. täältä https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812
+Config.Avatari = "" --Botin profiilikuva
+
+
+Config.Paivitysaika = 7 -- Kuinka monen sekunnin välein päivittyy
